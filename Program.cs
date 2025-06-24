bfd3621a21173be30a650e63279a60c7695da086
erensalihler <erensalihler@TCMX-YZLM-ES>
2025-06-25 00:32:07 +0300

    .

--- Program.cs
+++ Program.cs
@@ -3,4 +3,8 @@ DivideTwoIntegers sa = new DivideTwoIntegers();
 TwoSumClass twosum = new();
 int[] array = new[]{2,7,11,15};
 
-Console.WriteLine(twosum.TwoSum(array, 9));
+foreach (var item in twosum.TwoSum(array, 9))
+{
+    Console.WriteLine(item);
+}
+
